37088992813c5a1718a4e4c7050fe2502fffa07c
Eemeli Kelokorpi <eemeli.kelokorpi@yle.fi>
2015-04-11 16:25:13 +0300

    Update

--- main.js
+++ main.js
@@ -1,5 +1,34 @@
 window.onload = function() {
+	var browser = document.getElementById('browser');
+	var editor;
+	var changeTimer;
+
+	var initEditor = function(value) {
+		editor = ace.edit('editor');
+
+		editor.commands.addCommand({
+		    name: 'saveChanges',
+		    bindKey: { mac: 'Cmd-S', win: 'Ctrl-S' },
+		    exec: function() {
+		    	saveChanges();
+		    }
+		});
+
+		editor.getSession().setValue(value);
+		editor.focus();
+		saveChanges();
+
+		editor.getSession().on('change', function(event) {
+			if (changeTimer) clearTimeout(changeTimer);
+			changeTimer = setTimeout(function() {
+				saveChanges();
+			}, 1000);
+		});
+	};
+
 	var saveChanges = function() {
+		if (changeTimer) clearTimeout(changeTimer);
+		changeTimer = null;
 		browser.contentWindow.window.loaded = false;
 		browser.contentWindow.window.location.reload();
 		sendChanges();
@@ -13,34 +42,28 @@ window.onload = function() {
 			return;
 		}
 
-		var value = editor.getSession().getValue();
-		value = value.replace(/(\r\n|\n|\r)/gm, '');
+		var header = "game.module('game.main').body(function() {";
+		var footer = '});'
+		value = header + editor.getSession().getValue() + footer;
 		browser.contentWindow.runScript(value);
 	};
 
-	var browser = document.getElementById('browser');
-	editor = ace.edit('editor');
+	var user = 'ekelokorpi';
+	var id = 'WbqyVO';
 
-	editor.commands.addCommand({
-	    name: 'saveChanges',
-	    bindKey: { mac: 'Cmd-S', win: 'Ctrl-S' },
-	    exec: function() {
-	    	saveChanges();
-	    }
-	});
-
-	var file = document.location.href.split('?');
-	file = file[1] || 'default';
+	var href = document.location.href.split('?');
+	if (href[1]) href = href[1].split('&');
+	if (href[1]) {
+		user = href[0];
+		id = href[1];
+	}
 	
 	var request = new XMLHttpRequest();
 	request.onload = function() {
-		if (request.responseText) {
-			editor.getSession().setValue(request.responseText);
-			saveChanges();
+		if (request.responseText && request.status === 200) {
+			initEditor(request.responseText);
 		}
 	};
-	request.open('GET', 'snippets/' + file + '.js', true);
+	request.open('GET', 'http://codepen.io/' + user + '/pen/' + id + '.js', true);
 	request.send();
-
-	editor.focus();
 };
