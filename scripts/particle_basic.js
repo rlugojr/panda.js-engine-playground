99851c89e881edb8fdf9ed1c9f4be878d01a364a
Eemeli Kelokorpi <eemeli.kelokorpi@gmail.com>
2015-06-10 17:37:56 +0300

    Update

--- scripts/particle_basic.js
+++ scripts/particle_basic.js
@@ -1,33 +1,12 @@
 game.addAsset('particle.png');
 
-game.createClass('StarEmitter', 'Emitter', {
-	textures: ['particle.png'],
-	speed: 20,
-	speedVar: 10,
-	angleVar: Math.PI,
-	rate: 10,
-	endAlpha: 0,
-	// Custom properties
-	rotation: 0,
-	rotateSpeed: 2,
-	distFromCenter: 100,
-
-	init: function() {
-		this.addTo(game.scene.stage);
-		game.scene.addEmitter(this);
-		this.update();
-	},
-
-	update: function() {
-		// Rotate emitter position around center of canvas
-		this.position.x = game.width / 2 + Math.sin(this.rotation) * this.distFromCenter;
-		this.position.y = game.height / 2 + Math.cos(this.rotation) * this.distFromCenter;
-		this.rotation += this.rotateSpeed * game.delta;
-	}
-});
-
 game.createScene('Main', {	
     init: function() {
-    	this.emitter = new game.StarEmitter();
+    	var emitter = new game.Emitter();
+    	emitter.textures.push('particle.png');
+    	emitter.angleVar = Math.PI;
+    	emitter.position.set(game.width / 2, game.height / 2);
+    	emitter.addTo(this.stage);
+    	this.addEmitter(emitter);
     }
 });
